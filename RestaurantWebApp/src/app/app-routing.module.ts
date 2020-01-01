51cb1ad6e9490990afd0d32526fb389a70e9eb53
wrami <wramirez995@gmail.com>
2020-01-01 18:42:27 -0400

    Revert "Style and bootstrap added"
    This reverts commit b2af4453b0da86ae80cde009dbab560d2698409f.

--- RestaurantWebApp/src/app/app-routing.module.ts
+++ RestaurantWebApp/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DishesPageComponent } from './components/pages/dishes-page/dishes-page.component';
+import { RestaurantsPageComponent } from './components/pages/restaurants-page/restaurants-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dishes', component: DishesPageComponent },
+  { path: '', component: RestaurantsPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
