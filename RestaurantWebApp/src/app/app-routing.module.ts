b2af4453b0da86ae80cde009dbab560d2698409f
wrami <wramirez995@gmail.com>
2020-01-01 18:38:14 -0400

    Style and bootstrap added

--- RestaurantWebApp/src/app/app-routing.module.ts
+++ RestaurantWebApp/src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DishesPageComponent } from './components/pages/dishes-page/dishes-page.component';
-import { RestaurantsPageComponent } from './components/pages/restaurants-page/restaurants-page.component';
 
 
-const routes: Routes = [
-  { path: 'dishes', component: DishesPageComponent },
-  { path: '', component: RestaurantsPageComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
