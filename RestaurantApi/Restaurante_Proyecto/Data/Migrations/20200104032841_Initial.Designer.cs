9ba91fb96688ade5dcf2eb4970b5e006f3e9cc31
wrami <wramirez995@gmail.com>
2020-01-04 00:10:25 -0400

    End of the project I guess

--- RestaurantApi/Restaurante_Proyecto/Data/Migrations/20200104032841_Initial.Designer.cs
+++ RestaurantApi/Restaurante_Proyecto/Data/Migrations/20200104032841_Initial.Designer.cs
@@ -10,7 +10,7 @@ using Restaurante_Proyecto.Data;
 namespace Restaurante_Proyecto.Data.Migrations
 {
     [DbContext(typeof(Restaurante_ProyectoDbContext))]
-    [Migration("20191230212114_Initial")]
+    [Migration("20200104032841_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
