80322b7897fed6c03cb348e4c54befc793454017
wrami <wramirez995@gmail.com>
2020-01-08 21:22:59 -0400

    Defense done

--- RestaurantWebApp/src/app/components/layout/bodies/dish-showcase/dish-showcase.component.ts
+++ RestaurantWebApp/src/app/components/layout/bodies/dish-showcase/dish-showcase.component.ts
@@ -11,14 +11,34 @@ import { Dish } from 'src/app/models/dish';
 })
 export class DishShowcaseComponent implements OnInit {
   restaurants:Restaurant[];
-  dish:Dish;
+  dishes:Dish[];
 
   constructor(private restaurantService:RestaurantService, private dishService:DishService) { }
 
   ngOnInit() {
     this.restaurantService.getRestaurants().subscribe(restaurants => {
-      this.restaurants = restaurants;
+      this.restaurants = restaurants; 
+      this.restaurants.forEach(restaurant => {
+        this.dishService.getDishes(restaurant.id).subscribe( dishes => {
+          this.dishes = dishes;
+          this.dishes.sort(function(a, b) {
+            if (a.cost > b.cost) {
+                return -1;
+            }
+            if (b.cost > a.cost) {
+                return 1;
+            }
+            return 0;
+            })
+            
+          restaurant.dishes.push(
+            this.dishes[this.dishes.length-1]
+          );
+        });
+  
+      });
+      console.log(this.restaurants);
     });
-  }
 
+  }
 }
